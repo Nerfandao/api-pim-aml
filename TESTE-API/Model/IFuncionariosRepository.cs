3f61de22a1a09bc2e9ca55a02f0110c92318e049
Fernando Gigliotti <pucca.bruzil@hotmail.com.br>
2023-09-19 13:40:29 -0300

    Feet: Add Função de login com JTW

---
+++ TESTE-API/Model/IFuncionariosRepository.cs
@@ -0,0 +1,12 @@
+﻿using TESTE_API.Entities;
+
+namespace TESTE_API.Model
+{
+    public interface IFuncionariosRepository
+    {
+        void Add(Entities.Funcionarios funcionarios);
+        void Delete(int id);
+        List<Entities.Funcionarios> Get();
+        void Update(Entities.Funcionarios funcionario);
+    }
+}
